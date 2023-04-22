c2bd18a095d6edb039226cc82006930a0878dea7
Subhodip Sardar <sardarsubhodip857@gmail.com>
2023-04-22 19:37:04 +0530

    Setup file has added

---
+++ setup.py
@@ -0,0 +1,14 @@
+import platform
+import os
+os_name=platform.system()
+if os_name=="Windows":
+    os.system("pip install pyttsx3")
+    os.system("pip install wikipedia")
+    os.system("pip install pyautogui")
+    os.system("pip install SpeechRecognition")
+elif os_name=="Linux" :
+    os.system("pip3 install pyttsx3")
+    os.system("pip3 install wikipedia")
+    os.system("pip3 install pyautogui")
+    os.system("pip3 install SpeechRecognition")
+    
